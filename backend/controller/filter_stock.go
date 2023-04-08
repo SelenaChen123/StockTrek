6a1e5c504780ce865c44abd49d6b473ead722f1f
piepielovers <tboywu@gmail.com>
2023-04-08 19:11:16 -0400

    finished getting stock values of timeframe

---
+++ backend/controller/filter_stock.go
@@ -0,0 +1,81 @@
+package controller
+
+import (
+	"context"
+	"net/http"
+	"stock-treck-api/configs"
+	"stock-treck-api/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var stocksCollection *mongo.Collection = configs.GetCollection(configs.DB, "stocks")
+
+const (
+	layout = "2006-01-02"
+)
+
+func FilterStock() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Parse URL param
+		company := c.Param("company")
+		var stock models.Stock
+		filter := bson.M{"company": company}
+		err := stocksCollection.FindOne(ctx, filter).Decode(&stock)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Company not found."})
+			return
+		}
+
+		// Check if query params exist
+		start := c.Query("startDate")
+		end := c.Query("endDate")
+		if len(start) == 0 || len(end) == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Empty query parameters.",
+			})
+			return
+		}
+
+		// Parse query param start date
+		startDate, err := time.Parse(layout, start)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid start date.",
+			})
+			return
+		}
+		// Parse query param end date
+		endDate, err := time.Parse(layout, end)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid end date.",
+			})
+			return
+		}
+
+		// Calculate how many stocks to retrieve
+		var numDays int = int(endDate.Sub(startDate).Hours()/24) + 1
+		var stockValues []models.StockValue = make([]models.StockValue, 0, numDays)
+
+		// Get timeframe of stock values
+		for _, value := range stock.Values {
+			if start <= value.Date && value.Date <= end {
+				stockValues = append(stockValues, value)
+			}
+		}
+
+		// Return company stock information
+		c.JSON(http.StatusOK, gin.H{
+			"name":   stock.Company,
+			"ticker": stock.Ticker,
+			"data":   stockValues,
+		})
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -12,11 +12,7 @@ func main() {
 
 	configs.ConnectDB()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "asdfasdfasdf",
-		})
-	})
-	router.GET("/stock/:stockName", controller.GetStockData())
+	router.GET("/stock/:company", controller.FilterStock())
+
 	router.Run()
 }
