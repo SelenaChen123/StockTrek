c9190aee043e6d06905ddb94f42d93b7c0d16d48
piepielovers <tboywu@gmail.com>
2023-04-08 16:50:34 -0400

    removed duplicates

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"stock-treck-api/configs"
+	"stock-treck-api/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	ConnectDB()
+	configs.ConnectDB()
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "asdfasdfasdf",
 		})
 	})
-	router.GET("/stock/:stockName", GetStockData())
+	router.GET("/stock/:stockName", controller.GetStockData())
 	router.Run()
 }
